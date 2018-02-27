d47e7dedcfca94831e30c0c5becce3c956777ef8
MalakhSecturn <36860512+MalakhSecturn@users.noreply.github.com>
2018-02-27 13:06:16 -0500

    CHANGE #2

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es-CO">
+<head>
+	<meta charset="UTF-8">
+	<title>App para sacar formulas de quimica</title>
+	<!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link rel="stylesheet" href="css/style.css">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+</head>
+<body>
+  
+</body>
+</html>
