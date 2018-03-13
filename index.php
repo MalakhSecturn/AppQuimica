3f2d6be92d58b0105067ed8ab42b6a5e328629e7
MalakhSecturn <36860512+MalakhSecturn@users.noreply.github.com>
2018-03-13 14:25:35 -0500

    ACTUALIZACIÓN

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="es-CO">
-<head>
-	<meta charset="UTF-8">
-	<title>App para sacar formulas de quimica</title>
-	<!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-      <link rel="stylesheet" href="css/style.css">
-
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-			<meta name="description" content="Valancea equaciones quimicas, encuentra masa empirica etc">
-			<meta name="keywords" content="quimica, nivelar, equaciones quimicas, masa empirica, formula empirica">
-    </head>
-<body>
+<?php 
+	$page_name = "Home";
+	require 'Templates/header.php';
+?>
  <h1 align=CENTER >quimica grado 10º y 11º</h1> <br> 
  <hr>
   <i> <strong>ESCRIBE CUANTOS ELEMENTOS TIENE EL COMPUESTO A VALANCEAR</strong> </i> 
@@ -32,13 +19,6 @@
     }
 
    </script>
-=======
-=======
->>>>>>> 750e333f8912acc5d1952f1ff59a43f9db7f983e
-<?php 
-	$page_name = "Home";
-	require 'Templates/header.php';
-?>
 	<div class="container">
 		<div class="row">
 			<form action="verification.php">
@@ -49,9 +29,5 @@
 			</form>
 		</div>
 	</div>
-<<<<<<< HEAD
->>>>>>> 750e333f8912acc5d1952f1ff59a43f9db7f983e
-=======
->>>>>>> 750e333f8912acc5d1952f1ff59a43f9db7f983e
 </body>
 </html>
