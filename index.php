4e8ce2f06d1e322bf52987adc2572120d119635c
MalakhSecturn <36860512+MalakhSecturn@users.noreply.github.com>
2018-03-13 14:47:14 -0500

    #6

--- index.php
+++ index.php
@@ -7,21 +7,56 @@
 			<h1 class="center-align">Quimica Grado 10° y 11°</h1>
 		</header>
 		<div class="row">
-			<div class="col s6">
-				<h5 class="center-align">Deseas agregar mas elementos</h5>
-				<button class="btn-floating btn-large waves-effect waves-light red">+</button>
+			<div class="col s12">
+				<h5 class="center-align">Deseas agregar mas elementos <button class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></button></h5>
+				
 			</div>
 		</div>
 	</div>
 	<div class="container">
-		<div class="row">
-			<form action="verification.php">
-				<div class="col s12 input-field">
+		<form action="verification.php">
+			<!-- ELEMENTO DEL COMPUESTO -->
+			<div class="row">
+				<div class="col s1"></div>
+				<div class="col s5 input-field">
 					<input type="text" class="validate" name="insert_compound" id="insert_compound" placeholder="Insertar compuesto">
 				</div>
-				<input type="submit" class="waves-effect waves-light col s12 btn" value="MOSTRAR ECUACIONES">
-			</form>
-		</div>
+				<div class="col s5 input-field">
+					<input type="text" class="validate" name="insert_compound" id="insert_compound" placeholder="Insertar compuesto">
+				</div>
+				<div class="col s1"></div>
+			</div>
+			<!-- FIN DEL ELEMENTO -->
+			<!-- ELEMENTO DEL COMPUESTO -->
+			<div class="row">
+				<div class="col s1"></div>
+				<div class="col s5 input-field">
+					<input type="text" class="validate" name="insert_compound" id="insert_compound" placeholder="Insertar compuesto">
+				</div>
+				<div class="col s5 input-field">
+					<input type="text" class="validate" name="insert_compound" id="insert_compound" placeholder="Insertar compuesto">
+				</div>
+				<div class="col s1"></div>
+			</div>
+			<!-- FIN DEL ELEMENTO -->
+			<!-- ELEMENTO DEL COMPUESTO -->
+			<div class="row">
+				<div class="col s1"></div>
+				<div class="col s5 input-field">
+					<input type="text" class="validate" name="insert_compound" id="insert_compound" placeholder="Insertar compuesto">
+				</div>
+				<div class="col s5 input-field">
+					<input type="text" class="validate" name="insert_compound" id="insert_compound" placeholder="Insertar compuesto">
+				</div>
+				<div class="col s1"></div>
+			</div>
+			<!-- FIN DEL ELEMENTO -->
+			<div class="row">
+				<div class="col s1"></div>
+				<input type="submit" class="waves-effect waves-light col s10 btn" value="MOSTRAR ECUACIONES">
+				<div class="col s1"></div>
+			</div>
+		</form>
 	</div>
 	<script> 
 	function  cp1() {
