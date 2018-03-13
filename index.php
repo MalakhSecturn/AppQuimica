29db335865601a94e6e87daef121c4427ea80dd7
samuelfelipe <samuelfelipecorrea@gmail.com>
2018-03-13 14:17:10 -0500

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="es-CO">
 <head>
@@ -11,9 +12,42 @@
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+			<meta name="description" content="Valancea equaciones quimicas, encuentra masa empirica etc">
+			<meta name="keywords" content="quimica, nivelar, equaciones quimicas, masa empirica, formula empirica">
     </head>
-</head>
 <body>
+ <h1 align=CENTER >quimica grado 10º y 11º</h1> <br> 
+ <hr>
+  <i> <strong>ESCRIBE CUANTOS ELEMENTOS TIENE EL COMPUESTO A VALANCEAR</strong> </i> 
+  <input type="button" value="1" name="b1" onclick="cp1()"> 
+  <input type="button" value="2" name="b2"> 
+  <input type="button" value="3" name="b3"> 
+  <input type="button" value="4" name="b4"> 
+  <input type="button" value="5" name="b5"> <br>
   
+  <p id="lol"></p>
+  <script> 
+    function  cp1() {
+
+      document.getElementById("lol").innerHTML = "aqui va un input o dos o tres pero no se como hacerlo pl ayuda plox" 
+    }
+
+   </script>
+=======
+<?php 
+	$page_name = "Home";
+	require 'Templates/header.php';
+?>
+	<div class="container">
+		<div class="row">
+			<form action="verification.php">
+				<div class="col s12 input-field">
+					<input type="text" class="validate" name="insert_compound" id="insert_compound" placeholder="Insert the chemical compound">
+				</div>
+				<input type="submit" class="waves-effect waves-light col s12 btn" value="FIND COMPOUND AND YOU CHARACTERISTICS">
+			</form>
+		</div>
+	</div>
+>>>>>>> 750e333f8912acc5d1952f1ff59a43f9db7f983e
 </body>
 </html>
