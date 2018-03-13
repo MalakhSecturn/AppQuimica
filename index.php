85d1cd0ae285974dbff3739c50fd2e8fe7824b46
MalakhSecturn <36860512+MalakhSecturn@users.noreply.github.com>
2018-03-13 14:38:18 -0500

    #6

--- index.php
+++ index.php
@@ -6,14 +6,20 @@
 		<header>
 			<h1 class="center-align">Quimica Grado 10° y 11°</h1>
 		</header>
+		<div class="row">
+			<div class="col s6">
+				<h5 class="center-align">Deseas agregar mas elementos</h5>
+				<button class="btn-floating btn-large waves-effect waves-light red">+</button>
+			</div>
+		</div>
 	</div>
 	<div class="container">
 		<div class="row">
 			<form action="verification.php">
 				<div class="col s12 input-field">
-					<input type="text" class="validate" name="insert_compound" id="insert_compound" placeholder="Insert the chemical compound">
+					<input type="text" class="validate" name="insert_compound" id="insert_compound" placeholder="Insertar compuesto">
 				</div>
-				<input type="submit" class="waves-effect waves-light col s12 btn" value="FIND COMPOUND AND YOU CHARACTERISTICS">
+				<input type="submit" class="waves-effect waves-light col s12 btn" value="MOSTRAR ECUACIONES">
 			</form>
 		</div>
 	</div>
